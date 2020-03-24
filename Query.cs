253504382debf3c7d7aa556f02ba731bef22e1d4
Daniel Gut <daniel.gut@gia.ch>
2020-03-24 19:42:12 +0100

    feat(HC): inference bug

---
+++ Query.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+#nullable enable
+
+namespace InferenceRepro
+{
+    public class Query
+    {
+        public bool Wrong(List<string> values) => true;
+        public bool StillWrong(List<string>? values) => true;
+    }
+}
